96f5617bb13c4ceeda41619aa3284d113ac33dde
gyana <gyana.professional@gmail.com>
2020-12-01 17:19:23 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,19 +11,23 @@ import { catchError, map, tap } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
   title = 'spaceEXlaunch';
-  yFilterArr = ['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'];
+  yFilterArr = ['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'];
   spaceData: any = [];
   baseUrl = '';
+  focus = { yr: {}, lh: {}, ld: {} };
+  filterObj = { year: '', launch: '', land: '' };
   constructor(private http: HttpClient, ) { }
   ngOnInit(): void {
     this.baseUrl = 'https://api.spaceXdata.com/v3/launches?limit=100';
     this.getApiCall(this.baseUrl);
   }
-  urlMaker(year, launch, land): void {
+  urlMaker(key, id): void {
+    this.focus[key] = {};
+    this.focus[key][id] = true;
     let url = this.baseUrl;
-    if (launch !== undefined) { url = url + '&launch_success=' + launch; }
-    if (land !== undefined) { url = url + '&land_success=' + land; }
-    if (year !== undefined) { url = url + '&launch_year=' + year; }
+    if (this.filterObj.launch !== '') { url = url + '&launch_success=' + this.filterObj.launch; }
+    if (this.filterObj.land !== '') { url = url + '&land_success=' + this.filterObj.land; }
+    if (this.filterObj.year !== '') { url = url + '&launch_year=' + this.filterObj.year; }
     this.getApiCall(url);
   }
   getApiCall(url): any {
